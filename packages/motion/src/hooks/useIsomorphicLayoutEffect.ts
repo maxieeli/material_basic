24816a50a94e1c1543cacb146b45a88315094dda
owenli <owenlideveloper@gmail.com>
2022-09-25 15:52:03 +0800

    feat: update component

--- packages/motion/src/hooks/useIsomorphicLayoutEffect.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect, useLayoutEffect } from 'react'
-import canUseDom from 'rc-util/lib/Dom/canUseDom'
-
-const useIsomorphicLayoutEffect = canUseDom()
-  ? useLayoutEffect
-  : useEffect
-
-export default useIsomorphicLayoutEffect
